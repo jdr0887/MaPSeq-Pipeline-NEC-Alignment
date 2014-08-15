312e5fc0e2353aa8206afbdd878c83d5a1a97bde
Jason Reilly <jdr0887@renci.org>
2014-08-15 13:42:37 -0400

    0.5.x api changes

--- nec-alignment-messaging/src/test/java/edu/unc/mapseq/messaging/NECAlignmentMessageTest.java
+++ nec-alignment-messaging/src/test/java/edu/unc/mapseq/messaging/NECAlignmentMessageTest.java
@@ -22,9 +22,9 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.commons.lang.time.DateFormatUtils;
 import org.junit.Test;
 
-import edu.unc.mapseq.dao.model.HTSFSample;
-import edu.unc.mapseq.dao.model.SequencerRun;
-import edu.unc.mapseq.ws.HTSFSampleService;
+import edu.unc.mapseq.dao.model.Flowcell;
+import edu.unc.mapseq.dao.model.Sample;
+import edu.unc.mapseq.ws.SampleService;
 
 public class NECAlignmentMessageTest {
 
@@ -114,23 +114,23 @@ public class NECAlignmentMessageTest {
     @Test
     public void testQueueStress() {
 
-        QName serviceQName = new QName("http://ws.mapseq.unc.edu", "HTSFSampleService");
-        QName portQName = new QName("http://ws.mapseq.unc.edu", "HTSFSamplePort");
+        QName serviceQName = new QName("http://ws.mapseq.unc.edu", "SampleService");
+        QName portQName = new QName("http://ws.mapseq.unc.edu", "SamplePort");
         Service service = Service.create(serviceQName);
         String host = "biodev2.its.unc.edu";
         service.addPort(portQName, SOAPBinding.SOAP11HTTP_MTOM_BINDING,
-                String.format("http://%s:%d/cxf/HTSFSampleService", host, 8181));
-        HTSFSampleService htsfSampleService = service.getPort(HTSFSampleService.class);
+                String.format("http://%s:%d/cxf/SampleService", host, 8181));
+        SampleService sampleService = service.getPort(SampleService.class);
 
-        List<HTSFSample> sampleList = new ArrayList<HTSFSample>();
+        List<Sample> sampleList = new ArrayList<Sample>();
 
         // sampleList.addAll(htsfSampleService.findBySequencerRunId(191541L));
         // sampleList.addAll(htsfSampleService.findBySequencerRunId(191738L));
         // sampleList.addAll(htsfSampleService.findBySequencerRunId(190345L));
         // sampleList.addAll(htsfSampleService.findBySequencerRunId(190520L));
         // sampleList.addAll(htsfSampleService.findBySequencerRunId(191372L));
-        sampleList.addAll(htsfSampleService.findBySequencerRunId(192405L));
-        sampleList.addAll(htsfSampleService.findBySequencerRunId(191192L));
+        sampleList.addAll(sampleService.findByFlowcellId(192405L));
+        sampleList.addAll(sampleService.findByFlowcellId(191192L));
 
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(String.format("nio://%s:61616",
                 "biodev2.its.unc.edu"));
@@ -143,14 +143,14 @@ public class NECAlignmentMessageTest {
             MessageProducer producer = session.createProducer(destination);
             producer.setDeliveryMode(DeliveryMode.PERSISTENT);
             String format = "{\"account_name\":\"%s\",\"entities\":[{\"entity_type\":\"HTSFSample\",\"guid\":\"%d\"},{\"entity_type\":\"WorkflowRun\",\"name\":\"%s_L%d_%s_BWA\"}]}";
-            for (HTSFSample sample : sampleList) {
+            for (Sample sample : sampleList) {
 
                 if ("Undetermined".equals(sample.getBarcode())) {
                     continue;
                 }
 
-                SequencerRun sr = sample.getSequencerRun();
-                String message = String.format(format, "rc_renci.svc", sample.getId(), sr.getName(),
+                Flowcell flowcell = sample.getFlowcell();
+                String message = String.format(format, "rc_renci.svc", sample.getId(), flowcell.getName(),
                         sample.getLaneIndex(), sample.getName());
                 System.out.println(message);
                 producer.send(session.createTextMessage(message));
